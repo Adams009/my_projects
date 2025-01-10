5c03b33f3a96af15d303291bfe38850f2fab0f46
Adams009 <olaadam003@gmail.com>
2025-01-10 20:53:57 +0100

    testing the path modules

--- node-basic/working-with-path.js
+++ node-basic/working-with-path.js
@@ -4,3 +4,8 @@ console.log(path.dirname(__filename)); // console dirname
 
 console.log(path.basename(__filename)); // console filename
 
+console.log(path.extname(__filename)); // console extension
+
+console.log(path.parse(__filename)); // console root, dir, basename, extension, filename
+
+console.log(path.join(__dirname, 'test', 'hello.txt')); // console join path
