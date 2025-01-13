8cab1a8af00015fce678d1b9ea5623afeda3dcd3
Adams009 <olaadam003@gmail.com>
2025-01-13 10:38:55 +0100

    create a server and routes with http module

--- node-basic/working-with-http.js
+++ node-basic/working-with-http.js
@@ -1,10 +1,28 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-    console.log(req);
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end("OK");
+// const server = http.createServer((req, res) => {
+//     console.log(req);
+//     res.writeHead(200,{'Content-Type': 'text/plain'});
+//     res.end("OK");
     
+// })
+
+// server.listen(3000, () => {
+//     console.log("listening on port 3000");
+// });
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    if (url === '/') {
+        res.writeHead(200,{'Content-Type': 'text/plain'});
+        res.end("Welcome to the HomePage");
+    } else if (url === '/register') {
+        res.writeHead(200,{'Content-Type': 'text/plain'});
+        res.end("Welcome to the Register Page");
+    } else {
+        res.writeHead(404,{'Content-Type': 'text/plain'});
+        res.end("Page Not Found");
+    }
 })
 
 server.listen(3000, () => {
